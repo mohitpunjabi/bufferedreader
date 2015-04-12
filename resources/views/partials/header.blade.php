96c15771b4ad5a151e33b67c7e4457e40b6860a2
Mohit Punjabi <me@mohitpunjabi.com>
2015-04-12 07:04:11 +0530

    Fixed header empty values

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
-<header class="intro-header" style="background-image: url('{{ $background }}')">
+<header class="intro-header" style="background-image: url('{{ $background or '' }}')">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="site-heading">
-                    <h1>{{ $title }}</h1>
+                    <h1>{{ $title or '' }}</h1>
                     <hr class="small">
                     <span class="subheading">{{ $subtitle or '' }}</span>
                 </div>
