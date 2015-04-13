dd85e4452a375e791e4889f0f7f2ec201cd8767f
Mohit Punjabi <me@mohitpunjabi.com>
2015-04-13 23:32:39 +0530

    Added admin links

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -13,15 +13,17 @@
 
         <div class="collapse navbar-collapse" id="navbar">
         @unless(Auth::guest())
-                <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            @endunless
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="{{ url('/issues') }}">All Issues</a></li>
+            </ul>
+        @endunless
 
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="{{ url('/') }}">Home</a></li>
